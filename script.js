edf5065a0c7756ef46dee340123857acc6beeb5b
sigvant <bnsviv@gmail.com>
2021-02-21 15:59:11 -0300

    final commit

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
 	x: 8 * box,
 	y: 8 * box
 }
+let score = 0;
 
 let direction = "right";
 let food = {
@@ -43,16 +44,21 @@ function update (event){
 	
 }
 
+// function scoreup (){
+// 	document.getElementById('score').text=score+10;
+// }
+
 function iniciarJogo(){
 
-	if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
-	if(snake[0].x < 1 && direction == 'left') snake[0].x = 16 * box;
-	if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
-	if(snake[0].y < 1 && direction == 'up') snake[0].y = 16 * box;
+	if(snake[0].x >= 15 * box && direction == 'right') snake[0].x = 0;
+	if(snake[0].x <= 1 && direction == 'left') snake[0].x = 16 * box;
+	if(snake[0].y >= 15 * box && direction == 'down') snake[0].y = 0;
+	if(snake[0].y <= 1 && direction == 'up') snake[0].y = 16 * box;
 
 	for(i=1 ; i < snake.length; i++){
 		if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
 			clearInterval(jogo);
+			document.getElementById('body').style.backgroundImage="url(pic.jpg)"
 			alert("GAME OVER")
 
 		}
@@ -72,6 +78,8 @@ function iniciarJogo(){
 
 	if(snakeX != food.x || snakeY != food.y){
 		snake.pop();
+		// scoreup();
+		
 	} else {
 		food.x = Math.floor(Math.random() * 15 + 1) * box;
 		food.y = Math.floor(Math.random() * 15 + 1) * box;
