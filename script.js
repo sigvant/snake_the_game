83a787d8333e1584360d04a1c24c0a327e10dc4f
sigvant <bnsviv@gmail.com>
2021-02-21 15:36:56 -0300

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-
+let snake = [];
 snake[0] = {
 	x: 8 * box,
 	y: 8 * box
@@ -21,7 +21,7 @@ function criarBG() {
 }
 
 function criarCobrinha(){
-	for(i=0, i < snake.length; i++){
+	for(i=0; i < snake.length; i++){
 		context.fillStyle = "green";
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
@@ -36,10 +36,10 @@ function drawFood(){
 document.addEventListener('keydown', update);
 
 function update (event){
-	if(event.keyCode == 37 && direction =! "right") direction = "left";
-	if(event.keyCode == 38 && direction =! "down") direction = "up";
-	if(event.keyCode == 39 && direction =! "left") direction = "right";
-	if(event.keyCode == 40 && direction =! "up") direction = "down";
+	if(event.keyCode == 37 && direction != "right") direction = "left";
+	if(event.keyCode == 38 && direction != "down") direction = "up";
+	if(event.keyCode == 39 && direction != "left") direction = "right";
+	if(event.keyCode == 40 && direction != "up") direction = "down";
 	
 }
 
@@ -48,7 +48,7 @@ function iniciarJogo(){
 	if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
 	if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
 	if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
-	if(snake[0].x < 0 && direction == 'up') snake[0].y = 16 * box;
+	if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
 	for(i=1 ; i < snake.length; i++){
 		if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
@@ -72,8 +72,8 @@ function iniciarJogo(){
 	if(snakeX != food.x || snakeY != food.y){
 		snake.pop();
 	} else {
-		food.x: Math.floor(Math.random() * 15 + 1) * box,
-		food.y: Math.floor(Math.random() * 15 + 1) * box
+		food.x = Math.floor(Math.random() * 15 + 1) * box;
+		food.y = Math.floor(Math.random() * 15 + 1) * box;
 	}	
 
 	let newHead = {
